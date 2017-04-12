516134fc6861b8f5ce87c84bdac0a62de00f736d
TECnologyj77 <thomas.carney007@gmail.com>
2017-04-11 22:54:38 -0700

    Got the data from the DB to show up in the arhive page.

--- app/js/archive/archiveController.js
+++ app/js/archive/archiveController.js
@@ -2,7 +2,9 @@ app.controller('ArchiveController', ['$scope', 'ArchiveService', '$log', '$windo
     function($scope, ArchiveService, $log, $window) {
         var vm = $scope;
 
-       $scope.archive = ArchiveService.getArchivePodcasts();
+      ArchiveService.getArchivePodcasts().then(function(promise){
+          $scope.archive = promise.data;
+      });
 
         vm.filter = function(key) {
             $log.log('Requested filtering on key: "' + key + '"');
@@ -13,10 +15,7 @@ app.controller('ArchiveController', ['$scope', 'ArchiveService', '$log', '$windo
 
 app.factory('ArchiveService', ['UrlService', function(UrlService) {
     var getArchivePodcasts = function() {
-        UrlService.getArchivePodcasts().then(function(promise) {
-            console.log(promise.data);
-            return promise.data;
-        })
+        return UrlService.getArchivePodcasts();
     };
     return {
         getArchivePodcasts: getArchivePodcasts
